cc6b8ccff20a4e2fd4e4b7fc7a9662ec7f615bb6
Edean <edeand1@gmail.com>
2018-09-10 21:05:41 -0700

    centered

--- public/src/components/draw.jsx
+++ public/src/components/draw.jsx
@@ -3,16 +3,20 @@ import ReactDOM from "react-dom";
 import CanvasDraw from "./drawArea";
 import "../pages/css/utilities.css";
 import SketchCanvas from "./game";
-const pushit = {
-    top: "50%"
+const centerCanvas = {
+    height: "100vh",
+    alignItems: "center",
+    justifyContent: "middle",
+    display: "flex"
+
 }
 class drawArea extends React.Component {
     render() {
         return (
-        <div className="row">
+        <div className="row" style={centerCanvas}>
             <div className="col-12">
-                <div className="text-center">
-                    <div ><CanvasDraw /></div>);
+                <div>
+                    <div className="text-center"><CanvasDraw /></div>);
                 </div>
             </div>
         </div>
