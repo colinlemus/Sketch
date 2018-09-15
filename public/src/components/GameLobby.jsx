8b2a7e722ff8625128023018b9fa8a30146d59d8
colin@thelemus.com <Colin@Colins-MacBook-Pro.local>
2018-09-15 13:07:42 -0700

    lit

--- public/src/components/GameLobby.jsx
+++ public/src/components/GameLobby.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import '../pages/css/SketchLogin.css';
 import '../pages/css/utilities.css';
-import UserProfile from './UserProfile';
-import { Link } from 'react-router-dom'
-import axios from 'axios';
+import gear from '../assests/gear.png';
+
+const navbar = {
+    width: "100%"
+}
 
 export default class SketchLobby extends React.Component {
     constructor(props, context) {
@@ -15,10 +17,17 @@ export default class SketchLobby extends React.Component {
 
     render() {
         return (
-            <div className='container'>
-                <div className='row'>
-                    {sessionStorage.getItem("username")}
-                </div>
+            <div>
+                <nav className="navbar navbar-expand-lg navbar-light bg-light" style={navbar}>
+                <a className="navbar-brand" href="#">
+                    <img src="https://getbootstrap.com/docs/4.0/assets/brand/bootstrap-solid.svg" width="30" height="30" alt="" />
+                </a>
+                <div className="my-2 my-lg-0">
+                            <a className="navbar-brand" href="#">
+                                <img src={gear} width="30" height="30" alt={gear} />
+                            </a>
+                        </div>
+                </nav>
             </div>
         );
     }
