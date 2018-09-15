dd9b203372775a0fba996df508521adbaa04c585
colin@thelemus.com <Colin@Colins-MacBook-Pro.local>
2018-09-15 11:39:12 -0700

    lit

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,14 +1,3 @@
-<<<<<<< Updated upstream
-var path = require('path');
-
-module.exports = app => {
-	app.get('/', (req, res) => {
-		res.sendFile(path.join(__dirname, '../', 'public', 'build', 'index.html'));
-	});
-
-	app.get('*', (req, res) => {
-=======
-var db = require('../models');
 var path = require('path');
 
 module.exports = function(app) {
@@ -17,7 +6,6 @@ module.exports = function(app) {
 	});
 
 	app.get('*', function(req, res) {
->>>>>>> Stashed changes
 		res.sendFile(path.join(__dirname, '../', 'public', 'build', 'index.html'));
 	});
 };
