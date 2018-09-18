2ad562ab2ad914a651478a59292018665e7c8273
Jason Rose <jrose@riotgames.com>
2018-09-17 18:56:38 -0700

    updates some stuff for websockets

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -4,8 +4,4 @@ module.exports = function(app) {
 	app.get('/', function(req, res) {
 		res.sendFile(path.join(__dirname, '../', 'public', 'build', 'index.html'));
 	});
-
-	app.use((req, res, next) => {
-		res.sendFile(path.join(__dirname, '../', 'public', 'build', 'index.html'));
-	});
 };
